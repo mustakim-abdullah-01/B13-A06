5525d649c61a8aed8e2fcaa203a33228b5282a5e
Mustakim Abdullah <mail.mustakim@yahoo.com>
2026-06-08 02:06:56 +0600

    Project Started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-8xl font-black flex justify-center items-center h-dvh">Hello</p>
+    </>
+  );
+}
+
+export default App;
